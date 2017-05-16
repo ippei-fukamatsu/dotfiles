c024795823f310b180264bd69d7252d474b79cef
Ippei Fukamatsu <ippei.fukamatsu@yume-lab.co.jp>
2017-05-16 11:37:08 +0900

    バグシュウセイ

--- setup.sh
+++ setup.sh
@@ -1,24 +1,20 @@
 cd ~;
 rm .vimrc .gvimrc .zshrc;
 
-ln -s ~/.dotfiles/_vimrc .vimrc;
-ln -s ~/.dotfiles/_gvimrc .gvimrc;
-ln -s ~/.dotfiles/_zshrc .zshrc;
-
 # install zsh
 brew install zsh
 
 # install oh-my-zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
-#source .bash_profile;
-source .zshrc;
-
 # install neobundle
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh;
 sh ./install.sh;
 rm -rf install.sh;
 
+ln -s ~/.dotfiles/_vimrc .vimrc;
+ln -s ~/.dotfiles/_gvimrc .gvimrc;
+ln -s ~/.dotfiles/_zshrc .zshrc;
 source ~/.zshrc
 
 echo "done.";
